cc21cc5a506162dac4b62a7214d6bda49ca890d9
nghiepit <me@nghiepit.dev>
2022-05-12 22:51:53 +0700

    jsdoc: Add default value

--- lib/types.ts
+++ lib/types.ts
@@ -1,8 +1,19 @@
 export interface Options {
+  /** @default false */
   deep?: boolean;
+
+  /** @default false */
   skipNull?: boolean;
+
+  /** @default true */
   emptyStrings?: boolean;
+
+  /** @default true */
   emptyInvalidNumbers?: boolean;
+
+  /** @default false */
   emptyArrays?: boolean;
+
+  /** @default false */
   emptyObjects?: boolean;
 }
